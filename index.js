e9c12643654a514954c4bac5bc54662929149cd9
johancvb <35685184+johancvb@users.noreply.github.com>
2021-03-12 09:37:56 +0100

    Data renderen op overview page mislukt, ejs error?

--- index.js
+++ index.js
@@ -14,21 +14,11 @@ app.set('views', 'views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	// Send a plain string using res.send();
 	res.redirect('/teams');
 });
 
 // Create a route for our overview page
 app.get('/teams', function (req, res) {
-	// request('https://free-nba.p.rapidapi.com/teams', {json: true}, function (err, requestRes, body){
-	// 	if (err) {
-	// 		// We got an error
-	// 		res.send(err);
-	// 	} else {
-	// 		// Render the page using the 'posts' view and our body data
-
-	// 	}
-	// });
 
 	axios.get('https://free-nba.p.rapidapi.com/teams', {
 		headers: {
@@ -41,53 +31,14 @@ app.get('/teams', function (req, res) {
 			
 			res.render('teams', {
 				title: 'Teams', // We use this for the page title, see views/partials/head.ejs
-				postData: response.data.data[0].name
+				data: response.data.data //[0].name
 			});
 		})
 		.catch(function (error) {
 			// handle error
 			console.log(error);
 		});
-
-	// Make a request for a user with a given ID
-	// axios.get('https://free-nba.p.rapidapi.com/teams', {
-
-	// })
-	// 	.then(function (response) {
-	// 		// handle success
-	// 		console.log(response);
-	// 	})
-	// 	.catch(function (error) {
-	// 		// handle error
-	// 		console.log(error);
-	// 	})
-
-
-
-
 });
-
-// // Create a route for our detail page
-// app.get('/post/:id', function(req, res) {
-// 	request(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`, {json: true}, function (err, requestRes, body){
-// 		if (err) {
-// 			// We got an error
-// 			res.send(err);
-// 		} else {
-// 			// Render the page using the 'post' view and our body data
-// 			res.render('post', {
-// 				title: `Post ${req.params.id}`, 
-// 				postData: body
-// 			});
-// 		}
-// 	});
-// });
-
-// // Make sure to catch /post to /posts
-// app.get('/post', function(req, res) {
-// 	// Redirect the client using res.redirect (this will create a new request)
-// 	res.redirect('/posts');
-// });
 
 // Actually set up the server
 app.listen(config.port, function () {
